f350f62defa1d012ba75a4d745a2257a1cdb53fc
JOAOGABRIEL8821 <145022962+JOAOGABRIEL8821@users.noreply.github.com>
2024-09-21 19:16:06 -0300

    Add files via upload

---
+++ assets/flashcard-imagens/readme.md
@@ -0,0 +1,17 @@
+## Cores
+
+```css
+:root {
+    --text-color: #DBE4EF;
+    --card-front-color: #144480;
+    --card-back-color: #00F4BF;
+}
+```
+
+> Para mais cores
+
+- https://colorhunt.co/
+- https://color.adobe.com/pt/create/color-wheel
+
+## Para fontes
+https://fonts.google.com/
---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criarCartao(
+    'Geografia',
+    'Qual a capital da frança?',
+    'capital da frança é paris',
+)
+
+criarCartao(
+    'Programação',
+    'oque é JS?',
+    'JS é uma linguagem de programação',
+)
+
+criarCartao(
+    'games',
+    'oque é o minecraft?',
+    'minecraft é um jogo de pc/celular',
+)
+
+criarCartao(
+    'games(minecraft)',
+    'oque o esqueleto segura?',
+    'Ele segura um arco e flecha, mas, pode segurar uma espada quando dropada',
+)
+
+criarCartao(
+    'games(mario)',
+    'quem é o mario?',
+    'Mario era um encanador, que foi sugado por uma cano, e seu objetivo é salvar a princesa',
+)
+
+criarCartao(
+    'games(rocket league)',
+    'Qual o melhor carro?',
+    'o melhor cara é o fennec',
+)
